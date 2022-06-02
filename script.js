e0e9174e7f444426262ca7cd0b1f549108a10506
marco95OP <41342819+marco95OP@users.noreply.github.com>
2022-06-02 18:07:08 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,26 +3,15 @@
 const hamburgerIcon = document.getElementById("hamburger-icon");
 const header = document.getElementById("header");
 const navLinks = document.querySelectorAll("nav-link");
-const div = document.querySelector("div");
+const nav = document.getElementsByTagName("nav");
 
+/*this function can close and open the hamburger*/ 
 hamburgerIcon.addEventListener("click", () => {
     header.classList.toggle("active");
 });
 
 navLinks.forEach(function (nav){
     nav.addEventListener("click", function(){
-        this.classList.toggle("open");
-
-        if(div.style.display === 'none'){
-            div.style.display = 'block';
-          } else{
-            div.style.display = 'none';
-          }
-        /* if (nav.classList.contains("open")){
-            navLinks.forEach(function (nav){
-                nav.classList.remove("open");
-            });
-            nav.classList.toggle("open");
-        } */
+        console.log(this);
     });
 });
